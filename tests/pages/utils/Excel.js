517ef4e542eaf2731efced1faac42cc916b7ea5a
Muniappan-M <munimurugan1410@gmail.com>
2026-07-17 15:46:30 +0530

    commit message

---
+++ tests/pages/utils/Excel.js
@@ -0,0 +1,12 @@
+import XLSX from "xlsx"
+
+function excelReader(){
+    const xlpath = "tests/pages/TestData/TestCredential.xlsx"      //Relative path
+       const workbook = XLSX.readFile(xlpath)                      //to read the xl file
+       const sheetname = workbook.SheetNames[0]                   // to locate the sheet using index
+       const sheet = workbook.Sheets[sheetname]
+       const xldata = XLSX.utils.sheet_to_json(sheet)             //to convert sheet to json
+       return xldata;
+
+       
+}
